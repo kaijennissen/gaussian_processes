4b6d434d0a265d6702d1aaf553504e8dac3a3c29
Kai Jennissen <kai.jennissen@statworx.com>
2020-01-04 11:30:04 +0100

    fit models with GPflow

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7.6-buster
+FROM tensorflow/tensorflow:nightly-py3-jupyter
 
 COPY requirements.txt /tmp/
 
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -t --rm -p 8888:8888 -v /Users/kj/Projekte/gaussian_processes:/app --name gp gp-docker:0.1
+docker run -t --rm -p 8888:8888 -v /Users/kj/Projekte/gaussian_processes:/tf/gp --name gp gp-docker:0.1
